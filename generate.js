80bac35d78f7a144a87c2595ed4b16caf9fa46f9
Mike Wooldridge <mike@hellion.org>
2021-06-01 17:14:32 -0700

    Update generate.js

--- generate.js
+++ generate.js
@@ -1,23 +1,15 @@
-// Run: node generate [optional config.json file]
-// config.source - Filename of entity source template
-// config.target - Filename of result to be written
-// config.props[].title - Entity titles to receive generate properties
-// config.props[].total - Total properties for those entities
-// config.types - Datatypes to cycle through when generating properties
-
 const CONFIG = "config.json";
 const fs  = require("fs");
 const myArgs = process.argv.slice(2);
 
 // Get config values
-let config = CONFIG;
-config = myArgs[0] !== undefined ? myArgs[0] : CONFIG;
+let config = myArgs[0] !== undefined ? myArgs[0] : CONFIG;
 config = fs.readFileSync('./' + config).toString();
 config = JSON.parse(config, null, " ");
 
 // Read source template
 source = fs.readFileSync('./' + config.source).toString();
-source = JSON.parse(source);
+result = JSON.parse(source);
 
 // Generate object of properties
 const getPropsObj = function (total) {
@@ -30,8 +22,7 @@ const getPropsObj = function (total) {
 	return obj;
 }
 
-let result = source;
-
+// Handle each entity definition that needs properties generated
 config.props.forEach((p, i) => {
 	let propsObj = getPropsObj(p.total);
 	let newProps = Object.assign(result.definitions[p.title].properties, propsObj);
